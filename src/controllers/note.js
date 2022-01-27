56209c28ac3f1d20ac437fc28dca7a04921b3233
Gibran Andre Ayala Cortes <gibranayala798@gmail.com>
2022-01-27 13:40:07 -0600

    Added /note/getoneid route

--- src/controllers/note.js
+++ src/controllers/note.js
@@ -4,14 +4,24 @@ const home = (req, res) => {
   res.status(200).send("Hello note")
 }
 
-const getAll= (req, res) => {
+const getAll = (req, res) => {
   Note.find({}, (err, docs) => {
     if (err) {
-      res.status(418).send("Error: " + err)
+      res.status(500).send("Error: " + err)
     } else {
       res.status(200).send(docs)
     }
   })
 }
 
-module.exports = { home, getAll }
+const getOneID = (req, res) => {
+  Note.findById(req.params.id, (err, docs) => {
+    if (err) {
+      res.status(500).send("Error: " + err)
+    } else {
+      res.status(200).send(docs)
+    }
+  })
+}
+
+module.exports = { home, getAll, getOneID }
