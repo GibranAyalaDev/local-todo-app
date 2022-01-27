bde75f1e931eb92044f4925044743de7f7f04e11
Gibran Andre Ayala Cortes <gibranayala798@gmail.com>
2022-01-27 11:18:56 -0600

    Added middlewars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // create the app
 const app = express()
 
+// middlewares
+app.use(morgan("dev"))
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+// Start the server
 app.listen(8000, () => {
   console.log("Server running on port 8000.")
 })
